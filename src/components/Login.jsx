52ddb281aaec96ce4d53d5e3acdf4dc1603d33d6
Lenovo <jannatjui414@gmail.com>
2025-01-07 14:00:17 +0600

    fixed login, register page margin

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -62,7 +62,7 @@ const Login = () => {
     }
     return (
         <div>
-            <div className="hero bg-base-200 min-h-screen">
+            <div className="hero bg-base-200 min-h-screen my-20">
                 <div className="hero-content flex-col lg:flex-row">
                     <div className="text-center lg:text-left">
                         <h1 className="text-5xl font-bold">Login now!</h1>
